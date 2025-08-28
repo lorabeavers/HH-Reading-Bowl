664c1b0e99ceeea2321c59076b201f99adf52277
lorabeavers <lorabeavers@gmail.com>
2025-08-28 11:01:28 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,57 @@
-const CACHE = 'rb-cards-v2';
-const ASSETS = ['./','./index.html','./manifest.json'];
+// Force a brand-new cache version whenever you change this string
+const CACHE = 'rb-cards-v3';
 
-self.addEventListener('install', (e)=>{
-  e.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
-});
-self.addEventListener('activate', (e)=>{ e.waitUntil(self.clients.claim()); });
-self.addEventListener('fetch', (e)=>{
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request).then(resp=>{
-      const copy = resp.clone();
-      caches.open(CACHE).then(cache=> cache.put(e.request, copy));
-      return resp;
-    }).catch(()=> caches.match('./index.html')))
+// List core assets to guarantee offline & pass install checks
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+// Install: pre-cache and take control ASAP
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
   );
+});
+
+// Activate: claim clients and remove any old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.map((k) => (k === CACHE ? null : caches.delete(k))));
+    await self.clients.claim();
+  })());
+});
+
+// Fetch: network-first for HTML, cache-first for everything else
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  const isHTML = req.headers.get('accept')?.includes('text/html');
+
+  if (isHTML) {
+    event.respondWith(
+      fetch(req).then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((c) => c.put(req, copy));
+        return res;
+      }).catch(() =>
+        caches.match(req).then((r) => r || caches.match('./index.html'))
+      )
+    );
+  } else {
+    event.respondWith(
+      caches.match(req).then((cached) => {
+        if (cached) return cached;
+        return fetch(req).then((res) => {
+          const copy = res.clone();
+          caches.open(CACHE).then((c) => c.put(req, copy));
+          return res;
+        }).catch(() => caches.match('./index.html'));
+      })
+    );
+  }
 });
